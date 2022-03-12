df497db8e08a6ff401c58a0fbc06ae003bffd278
Amit Patel <Amit.Patel1@fnb.co.za>
2022-03-12 15:06:00 +0200

    JUnit 5 Basics 16 - Using JUnit lifecycle hook annotations

--- src/test/java/com/example/demo/MathUtilsTest.java
+++ src/test/java/com/example/demo/MathUtilsTest.java
@@ -2,13 +2,21 @@ package com.example.demo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
-
+	
+	MathUtils mathUtils;
+	
+	@BeforeEach
+	void init() {
+		mathUtils = new MathUtils();
+	}
+	
 	@Test
 	void testAdd() {
-		MathUtils mathUtils = new MathUtils();
+		
 		int excpected = 2;
 		int actual = mathUtils.add(1, 1);
 		assertEquals(excpected,actual);
@@ -16,14 +24,14 @@ class MathUtilsTest {
 	
 	@Test
 	void testSqureNumber() {
-		MathUtils mathUtils = new MathUtils();
+		
 		int expected = 4;
 		assertEquals(expected,mathUtils.getSquareOfNumber(2),"The square method should multiple number");
 	}
 	
 	@Test
 	void testDivideNumber() {
-		MathUtils mathUtils = new MathUtils();
+		
 		assertThrows(ArithmeticException.class, () ->  mathUtils.getDivdeNumber(1, 0));
 		//assertThrows(NullPointerException.class, () ->  mathUtils.getDivdeNumber(1, 0));
 	}
