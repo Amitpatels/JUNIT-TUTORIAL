3c9569b4f7fc961a79d20a2d5c01d550f703347d
Amit Patel <Amit.Patel1@fnb.co.za>
2022-03-12 16:12:41 +0200

    JUnit 5 Basics 19 - Using DisplayName and Disabled annotations

--- src/test/java/com/example/demo/MathUtilsTest.java
+++ src/test/java/com/example/demo/MathUtilsTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
@@ -26,16 +28,17 @@ class MathUtilsTest {
 	}
 	
 	@BeforeAll
-	 void beforeAllProcess() {
+	static void beforeAllProcess() {
 		System.out.println("1. Before all processing start...");	
 	}
 	
 	@AfterAll
-	 void afterAllProcess() {
+	static void afterAllProcess() {
 		System.out.println("4. After all processing start...");	
 	}
 	
 	@Test
+	@DisplayName("This method will add two number")
 	void testAdd() {
 		
 		int excpected = 2;
@@ -57,4 +60,9 @@ class MathUtilsTest {
 		//assertThrows(NullPointerException.class, () ->  mathUtils.getDivdeNumber(1, 0));
 	}
 
+	@Test
+	@Disabled
+	void textDisabeld() {
+		System.out.println("Test disabled...");
+	}
 }
